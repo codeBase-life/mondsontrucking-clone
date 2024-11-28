55fc8df0f82f1bd6a87d3eae8856d56f199f9405
malik rasheed <codedev366@gmail.com>
2024-11-28 14:27:07 +0500

    fixing animation

---
+++ script.js
@@ -0,0 +1,130 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".box", {
+  scrollTrigger: ".box", // start animation when ".box" enters the viewport
+  y: 100,
+  // opacity: 0,
+  duration: 3,
+});
+
+gsap.from(".box1", {
+  scrollTrigger: ".box1", // start animation when ".box" enters the viewport
+  y: 200,
+  opacity: 0,
+  duration: 3,
+});
+gsap.from(".box2", {
+  scrollTrigger: ".box2", // start animation when ".box" enters the viewport
+  y: 100,
+  opacity: 0,
+  duration: 2,
+});
+
+gsap.from(".box3", {
+  scrollTrigger: ".box3", // start animation when ".box" enters the viewport
+  y: 50,
+  opacity: 0,
+  duration: 1,
+});
+
+gsap.from(".box4", {
+  scrollTrigger: ".box4", // start animation when ".box" enters the viewport
+  y: 50,
+  opacity: 0,
+  duration: 2,
+});
+
+gsap.from(".box5", {
+  scrollTrigger: ".box5", // start animation when ".box" enters the viewport
+  y: 50,
+  opacity: 0,
+  duration: 3,
+});
+gsap.from(".box6", {
+  scrollTrigger: ".box6", // start animation when ".box" enters the viewport
+  y: 50,
+  opacity: 0,
+  duration: 1,
+});
+gsap.from(".box7", {
+  scrollTrigger: ".box7", // start animation when ".box" enters the viewport
+  y: 50,
+  opacity: 0,
+  duration: 2,
+});
+gsap.from(".box8", {
+  scrollTrigger: ".box8", // start animation when ".box" enters the viewport
+  y: 50,
+  opacity: 0,
+  duration: 3,
+});
+
+gsap.from(".box9", {
+  scrollTrigger: ".box9", // start animation when ".box" enters the viewport
+  y: 50,
+  opacity: 0,
+  duration: 1,
+});
+gsap.from(".box10", {
+  scrollTrigger: ".box10", // start animation when ".box" enters the viewport
+  y: 50,
+  opacity: 0,
+  duration: 2,
+});
+gsap.from(".box11", {
+  scrollTrigger: ".box11", // start animation when ".box" enters the viewport
+  y: 50,
+  opacity: 0,
+  duration: 3,
+});
+
+gsap.from(".box12", {
+  scrollTrigger: ".box12", // start animation when ".box" enters the viewport
+  y: 50,
+  opacity: 0,
+  duration: 2,
+});
+gsap.from(".box13", {
+  scrollTrigger: ".box13", // start animation when ".box" enters the viewport
+  y: 50,
+  opacity: 0,
+  duration: 3,
+});
+
+gsap.from(".five-box1", {
+  scrollTrigger: ".five-box1", // start animation when ".box" enters the viewport
+  y: 50,
+  opacity: 0,
+  duration: 3,
+});
+
+gsap.from(".five-box2", {
+  scrollTrigger: ".five-box2", // start animation when ".box" enters the viewport
+  y: 30,
+  opacity: 0,
+  duration: 2,
+});
+gsap.from(".five-box3", {
+  scrollTrigger: ".five-box3", // start animation when ".box" enters the viewport
+  y: 30,
+  opacity: 0,
+  duration: 3,
+});
+gsap.from(".five-box4", {
+  scrollTrigger: ".five-box4", // start animation when ".box" enters the viewport
+  y: 30,
+  opacity: 0,
+  duration: 3,
+});
+gsap.from(".five-box5", {
+  scrollTrigger: ".five-box5", // start animation when ".box" enters the viewport
+  y: 30,
+  opacity: 0,
+  duration: 3,
+});
+gsap.from(".five-box6", {
+  scrollTrigger: ".five-box6", // start animation when ".box" enters the viewport
+  y: 30,
+  opacity: 0,
+  duration: 3,
+});
